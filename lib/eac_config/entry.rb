fca7a62e98fa0b875daccff8d13348ea4b821606
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-04-21 12:03:00 -0300

    EacConfig::Entry.

---
+++ lib/eac_config/entry.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'eac_config/entry_path'
+require 'eac_ruby_utils/core_ext'
+
+module EacConfig
+  class Entry
+    common_constructor :source_node, :path, :found, :value
+
+    def found?
+      found
+    end
+  end
+end
