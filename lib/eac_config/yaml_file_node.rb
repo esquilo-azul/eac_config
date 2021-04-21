0577376ca600eb6561b40ea4be0c49a8b7698789
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-04-20 22:16:11 -0300

    EacConfig::YamlFileNode.

---
+++ lib/eac_config/yaml_file_node.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'eac_config/entry'
+require 'eac_config/entry_search'
+require 'eac_config/readable_node'
+require 'eac_config/writeable_node'
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/yaml'
+
+module EacConfig
+  class YamlFileNode
+    include ::EacConfig::ReadableNode
+    include ::EacConfig::WriteableNode
+
+    common_constructor :path do
+      self.path = path.to_pathname
+    end
+
+    def data
+      @data ||= ::EacRubyUtils::Yaml.load_file(path)
+    end
+
+    def entry(*path)
+      ::EacConfig::EntrySearch.new(self, ::EacConfig::EntryPath.new(*path)).result
+    end
+  end
+end
