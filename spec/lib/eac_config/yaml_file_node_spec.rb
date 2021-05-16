7ac8d27465da2fa099255bbae96403d5b2e56a9d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-16 11:50:06 -0300

    EacConfig::Manager: remove.

--- spec/lib/eac_config/yaml_file_node_spec.rb
+++ spec/lib/eac_config/yaml_file_node_spec.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-require 'eac_config/manager'
 require 'eac_config/yaml_file_node'
 
-RSpec.describe ::EacConfig::Manager do
-  let(:fixtures_dir) { ::Pathname.new(__dir__).join('manager_spec_files') }
+RSpec.describe ::EacConfig::YamlFileNode do
+  let(:fixtures_dir) { ::Pathname.new(__dir__).join('yaml_file_node_spec_files') }
   let(:yaml_file_path) { fixtures_dir.join('storage1.yaml') }
-  let(:root_node) { ::EacConfig::YamlFileNode.new(yaml_file_path) }
-  let(:instance) { described_class.new(root_node) }
+  let(:instance) { described_class.new(yaml_file_path) }
 
   context 'with common entry' do
     let(:entry) { instance.entry('common') }
 
     it { expect(entry.value).to eq('AAA') }
-    it { expect(entry.source_node).to eq(root_node) }
+    it { expect(entry.source_node).to eq(instance) }
     it { expect(entry).to be_found }
   end
 
