f8658609beb7bc5590f74d2c6c51adf51651a558
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-16 10:35:40 -0300

    EacConfig::Node.url.

--- lib/eac_config/node.rb
+++ lib/eac_config/node.rb
@@ -7,10 +7,16 @@ module EacConfig
   module Node
     common_concern do
       enable_abstract_methods
+      include ::Comparable
     end
 
     def entry(path)
       ::EacConfig::EntrySearch.new(self, ::EacConfig::EntryPath.assert(path)).result
+    end
+
+    # @return [Addressable::URI]
+    def url
+      raise_abstract_method(__method__)
     end
   end
 end
