b072035be828b0314f84f72b4e6c53982272b246
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-16 13:48:59 -0300

    EacConfig::Node.self_loaded_nodes.

--- lib/eac_config/node.rb
+++ lib/eac_config/node.rb
@@ -10,6 +10,9 @@ module EacConfig
       include ::Comparable
     end
 
+    LOAD_PATH_ENTRY_PATH = ::EacConfig::EntryPath.assert(%w[load_path])
+    LOAD_PATH_PATH_SEPARATOR = ':'
+
     def entry(path)
       ::EacConfig::EntrySearch.new(self, ::EacConfig::EntryPath.assert(path)).result
     end
@@ -17,6 +20,20 @@ module EacConfig
     # @return [Addressable::URI]
     def url
       raise_abstract_method(__method__)
+    end
+
+    # @return [Array<EacConfig::Node>]
+    def self_loaded_nodes
+      ::EacConfig::EntrySearch.new(self, ::EacConfig::EntryPath.assert(LOAD_PATH_ENTRY_PATH))
+                              .result_from_self.if_present([]) do |e|
+        e.value.split(LOAD_PATH_PATH_SEPARATOR).map { |node_path| load_node(node_path) }
+      end
+    end
+
+    private
+
+    def load_node(node_path)
+      self.class.new(node_path.to_pathname.expand_path(path.parent))
     end
   end
 end
