b03e39ef11447347e1b9c00e8f658763c56b5843
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-20 12:44:23 -0300

    EacConfig::YamlFileNode: remove unnecessary requires.

--- lib/eac_config/yaml_file_node.rb
+++ lib/eac_config/yaml_file_node.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require 'addressable'
-require 'eac_config/entry'
-require 'eac_config/entry_search'
 require 'eac_config/node'
 require 'eac_ruby_utils/core_ext'
 require 'eac_ruby_utils/yaml'
