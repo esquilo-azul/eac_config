8998852e60ad968ea470c50da089de276a22453b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-12-05 11:18:45 -0300

    EacConfig::Rspec::Setup: restore envvar's load path value after example.

--- lib/eac_config/rspec/setup.rb
+++ lib/eac_config/rspec/setup.rb
@@ -7,11 +7,23 @@ module EacConfig
   module Rspec
     module Setup
       def self.extended(obj)
-        obj.clean_envvars_load_path
+        obj.rspec_config.around do |example|
+          obj.on_envvars_load_path_clean(example)
+        end
       end
 
-      def clean_envvars_load_path
-        ::EacConfig::EnvvarsNode.new.load_path.entry.value = []
+      def on_envvars_load_path_clean(example)
+        old_value = envvars_load_path_entry.value
+        begin
+          envvars_load_path_entry.value = old_value = []
+          example.run
+        ensure
+          envvars_load_path_entry.value = old_value
+        end
+      end
+
+      def envvars_load_path_entry
+        ::EacConfig::EnvvarsNode.new.load_path.entry
       end
     end
   end
