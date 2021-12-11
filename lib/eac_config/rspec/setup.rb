3f073b2c856fb149bf39f2c9d0a78b5e2adb55e0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-12-11 13:12:35 -0300

    EacConfig::Rspec::Setup.stub_eac_config_node.

--- lib/eac_config/rspec/setup.rb
+++ lib/eac_config/rspec/setup.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/fs/temp'
 require 'eac_config/envvars_node'
+require 'eac_config/node'
+require 'eac_config/yaml_file_node'
 
 module EacConfig
   module Rspec
@@ -24,6 +27,22 @@ module EacConfig
 
       def envvars_load_path_entry
         ::EacConfig::EnvvarsNode.new.load_path.entry
+      end
+
+      def stub_eac_config_node(&node_builder)
+        parent_self = self
+        rspec_config.around do |example|
+          ::EacRubyUtils::Fs::Temp.on_file do |file|
+            ::EacConfig::Node
+              .context.on(parent_self.stub_eac_config_node_build(file, &node_builder)) do
+              example.run
+            end
+          end
+        end
+      end
+
+      def stub_eac_config_node_build(file, &node_builder)
+        node_builder.present? ? node_builder.call(file) : ::EacConfig::YamlFileNode.new(file)
       end
     end
   end
